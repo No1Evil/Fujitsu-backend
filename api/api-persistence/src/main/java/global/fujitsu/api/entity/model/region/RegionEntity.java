86a29938f81a2bfa22031d071c31bbc6c31d2f3c
NotEvil <notevil036@gmail.com>
2026-03-21 19:24:08 +0200

    Revert "refactor(api-persistence): use WmoCode object instead of raw string"
    This reverts commit 0a034112bb9e90c18cd23f81289647074cf1eb5c.

--- api/api-persistence/src/main/java/global/fujitsu/api/entity/model/region/RegionEntity.java
+++ api/api-persistence/src/main/java/global/fujitsu/api/entity/model/region/RegionEntity.java
@@ -2,12 +2,16 @@ package global.fujitsu.api.entity.model.region;
 
 import global.fujitsu.api.entity.model.EntityModel;
 import global.fujitsu.api.model.region.RegionName;
-import global.fujitsu.api.model.region.WmoCode;
 import lombok.NonNull;
 
 public record RegionEntity(
     @NonNull Long id,
     @NonNull RegionName name,
-    @NonNull WmoCode wmoCode
+    @NonNull String wmoCode
 ) implements EntityModel {
+    public RegionEntity {
+        if (wmoCode.isBlank()){
+            throw new IllegalArgumentException("WMO code cannot be null");
+        }
+    }
 }
