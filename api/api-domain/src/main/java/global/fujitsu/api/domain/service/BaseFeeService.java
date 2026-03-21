a9925c84e4fce7d0f4ae21d02ae8b5bb36da305b
NotEvil <notevil036@gmail.com>
2026-03-22 00:18:35 +0200

    refactor(api-domain): renamed from BaseFeeService to TotalFeeService

--- api/api-domain/src/main/java/global/fujitsu/api/domain/service/BaseFeeService.java
+++
@@ -1,8 +0,0 @@
-package global.fujitsu.api.domain.service;
-
-import global.fujitsu.api.model.dto.request.BaseFeeRequest;
-import global.fujitsu.api.model.dto.response.BaseFeeResponse;
-
-public interface BaseFeeService {
-    BaseFeeResponse getBaseFee(BaseFeeRequest request);
-}
