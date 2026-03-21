766be652af959e86def85fe0a72e4994025aa397
NotEvil <notevil036@gmail.com>
2026-03-21 17:51:03 +0200

    feat(api-domain): implement RegionalBaseFeeService using BaseService

---
+++ api/api-domain/src/main/java/global/fujitsu/api/domain/service/RegionalBaseFeeService.java
@@ -0,0 +1,9 @@
+package global.fujitsu.api.domain.service;
+
+import global.fujitsu.api.domain.dto.request.create.CreateRegionalBaseFeeRequest;
+import global.fujitsu.api.domain.dto.response.get.RegionalBaseFeeResponse;
+import global.fujitsu.api.domain.service.base.BaseService;
+
+public interface RegionalBaseFeeService
+    extends BaseService<RegionalBaseFeeResponse, CreateRegionalBaseFeeRequest> {
+}
