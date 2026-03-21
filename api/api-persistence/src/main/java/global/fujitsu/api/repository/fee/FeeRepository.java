4030b40088ee11ccdaf052ec4a959466b4336e9e
NotEvil <notevil036@gmail.com>
2026-03-22 00:22:56 +0200

    refactor(api-persistence): made FeeRepository sealed to prevent inherit that interface

--- api/api-persistence/src/main/java/global/fujitsu/api/repository/fee/FeeRepository.java
+++ api/api-persistence/src/main/java/global/fujitsu/api/repository/fee/FeeRepository.java
@@ -3,5 +3,7 @@ package global.fujitsu.api.repository.fee;
 import global.fujitsu.api.entity.model.fee.EntityFeeModel;
 import global.fujitsu.api.repository.Repository;
 
-public interface FeeRepository<E extends EntityFeeModel> extends Repository<E> {
+public sealed interface FeeRepository<E extends EntityFeeModel>
+    extends Repository<E>
+    permits AirTemperatureFeeRepository, RegionalBaseFeeRepository, WeatherPhenomenonFeeRepository, WindSpeedFeeRepository {
 }
