73eea5e209998c08ff78c3d71566de1377ed4076
NotEvil <notevil036@gmail.com>
2026-03-23 01:55:22 +0200

    feat(business-logic): AirTemperatureFeeServiceImpl implementation matches AirTemperatureFeeService

--- implementations/business-logic/src/main/java/global/fujitsu/domain/service/fee/AirTemperatureFeeServiceImpl.java
+++ implementations/business-logic/src/main/java/global/fujitsu/domain/service/fee/AirTemperatureFeeServiceImpl.java
@@ -2,13 +2,11 @@ package global.fujitsu.domain.service.fee;
 
 import global.fujitsu.api.domain.exceptions.FeeNotFoundException;
 import global.fujitsu.api.domain.exceptions.RestrictedConditionException;
-import global.fujitsu.api.domain.service.AirTemperatureFeeService;
-import global.fujitsu.api.entity.model.fee.AirTemperatureFeeEntity;
+import global.fujitsu.api.domain.service.fee.AirTemperatureFeeService;
 import global.fujitsu.api.model.dto.request.create.CreateAirTemperatureFeeRequest;
+import global.fujitsu.api.model.dto.request.get.GetAirTemperatureFeeRequest;
 import global.fujitsu.api.model.dto.response.get.AirTemperatureFeeResponse;
 import global.fujitsu.api.model.fee.FeeResult;
-import global.fujitsu.api.model.region.RegionName;
-import global.fujitsu.api.model.vehicle.VehicleType;
 import global.fujitsu.api.repository.fee.AirTemperatureFeeRepository;
 import global.fujitsu.domain.mapper.impl.AirTemperatureFeeMapper;
 import lombok.NonNull;
@@ -16,29 +14,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AirTemperatureFeeServiceImpl implements AirTemperatureFeeService {
     private final AirTemperatureFeeRepository repository;
     private final AirTemperatureFeeMapper mapper;
 
     @Override
+    @Transactional
     public Long create(@NonNull CreateAirTemperatureFeeRequest request) {
         return repository.save(mapper.toEntity(request));
     }
 
     @Override
+    @Transactional
     public boolean delete(@NonNull Long id) {
         return repository.delete(id);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public AirTemperatureFeeResponse findById(@NonNull Long id) {
         var entity = repository.findById(id)
             .orElseThrow(() -> new FeeNotFoundException("Air temperature fee, with id {}, not found", id));
@@ -46,19 +43,17 @@ public class AirTemperatureFeeServiceImpl implements AirTemperatureFeeService {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public List<AirTemperatureFeeResponse> findAll() {
         return repository.findAll().stream().map(mapper::toResponse).toList();
     }
 
     @Override
-    @Transactional(readOnly = true)
-    public FeeResult getFee(@NonNull BigDecimal temperature) {
-        var feeResult = repository.findBaseFee(temperature)
+    public FeeResult getBaseFee(GetAirTemperatureFeeRequest request) {
+        var feeResult = repository.findBaseFee(request)
             .orElseThrow(() -> new FeeNotFoundException("Fee for air temperature not found"));
 
         if (!feeResult.isAllowed()){
-            throw new RestrictedConditionException("The temperature {} is not allowed", temperature);
+            throw new RestrictedConditionException("The temperature {} is not allowed", request.temperature());
         }
 
         return feeResult;
