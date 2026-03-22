b33261a1abc08221e151ff564f4b76409bda8fc3
NotEvil <notevil036@gmail.com>
2026-03-23 00:59:28 +0200

    feat(api-protocol): WeatherPhenomenonFeeService add generic class GetWeatherPhenomenonFeeRequest to extend interface BaseFeeService

--- api/api-domain/src/main/java/global/fujitsu/api/domain/service/fee/WeatherPhenomenonFeeService.java
+++ api/api-domain/src/main/java/global/fujitsu/api/domain/service/fee/WeatherPhenomenonFeeService.java
@@ -2,16 +2,12 @@ package global.fujitsu.api.domain.service.fee;
 
 import global.fujitsu.api.domain.service.base.BaseFeeService;
 import global.fujitsu.api.model.dto.request.create.CreateWeatherPhenomenonFeeRequest;
-import global.fujitsu.api.model.dto.request.get.GetWeatherPhenomenonRequest;
+import global.fujitsu.api.model.dto.request.get.GetWeatherPhenomenonFeeRequest;
 import global.fujitsu.api.model.dto.response.get.WeatherPhenomenonFeeResponse;
-import global.fujitsu.api.domain.service.base.BaseService;
-import global.fujitsu.api.model.fee.FeeResult;
-import global.fujitsu.api.model.vehicle.VehicleType;
-import lombok.NonNull;
 
 public interface WeatherPhenomenonFeeService extends BaseFeeService<
     WeatherPhenomenonFeeResponse,
     CreateWeatherPhenomenonFeeRequest,
-    GetWeatherPhenomenonRequest
+    GetWeatherPhenomenonFeeRequest
 > {
 }
