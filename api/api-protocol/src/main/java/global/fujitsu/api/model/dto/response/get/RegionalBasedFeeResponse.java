42c5f867a82249ca8be7a6a55f86880bb46c822b
NotEvil <notevil036@gmail.com>
2026-03-23 00:13:40 +0200

    refactor(api-protocol): rename from RegionalBaseFeeResponse to RegionalBasedFeeResponse

--- api/api-protocol/src/main/java/global/fujitsu/api/model/dto/response/get/RegionalBasedFeeResponse.java
+++ api/api-protocol/src/main/java/global/fujitsu/api/model/dto/response/get/RegionalBasedFeeResponse.java
@@ -1,12 +1,13 @@
 package global.fujitsu.api.model.dto.response.get;
 
+import global.fujitsu.api.model.dto.response.base.GetResponse;
 import global.fujitsu.api.model.region.RegionName;
 import global.fujitsu.api.model.vehicle.VehicleType;
 import lombok.NonNull;
 
 import java.math.BigDecimal;
 
-public record RegionalBaseFeeResponse(
+public record RegionalBasedFeeResponse(
     @NonNull Long id,
     @NonNull RegionName regionName,
     @NonNull VehicleType vehicleType,
