b30dde0a51f574fb7601613cc0d580ec0064d075
NotEvil <notevil036@gmail.com>
2026-03-23 00:32:47 +0200

    feat(api-domain): add VehicleTypeService interface

---
+++ api/api-domain/src/main/java/global/fujitsu/api/domain/service/VehicleTypeService.java
@@ -0,0 +1,15 @@
+package global.fujitsu.api.domain.service;
+
+import global.fujitsu.api.domain.service.base.BaseService;
+import global.fujitsu.api.model.dto.request.create.CreateVehicleTypeRequest;
+import global.fujitsu.api.model.dto.response.get.VehicleTypeResponse;
+import global.fujitsu.api.model.vehicle.VehicleType;
+import lombok.NonNull;
+
+public interface VehicleTypeService extends BaseService<
+    VehicleTypeResponse,
+    CreateVehicleTypeRequest
+> {
+
+    VehicleTypeResponse getByName(@NonNull VehicleType vehicleType);
+}
