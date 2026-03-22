c01fe3d819fb06067f5befb9bbc8029f61da50cc
NotEvil <notevil036@gmail.com>
2026-03-22 17:31:25 +0200

    refactor(api-persistence): RegionEntity removed unnecessary imports

--- api/api-persistence/src/main/java/global/fujitsu/api/entity/model/region/RegionEntity.java
+++ api/api-persistence/src/main/java/global/fujitsu/api/entity/model/region/RegionEntity.java
@@ -3,9 +3,11 @@ package global.fujitsu.api.entity.model.region;
 import global.fujitsu.api.entity.model.EntityModel;
 import global.fujitsu.api.model.region.RegionName;
 import global.fujitsu.api.model.region.WmoCode;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public record RegionEntity(
     @Nullable Long id,
