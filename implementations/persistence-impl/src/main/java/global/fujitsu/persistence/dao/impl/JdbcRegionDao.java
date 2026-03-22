5760d829fd0b3c1a71cab08bf14834f0263660fd
NotEvil <notevil036@gmail.com>
2026-03-22 17:47:27 +0200

    feat(persistence-impl): implemented JdbcRegionDao

---
+++ implementations/persistence-impl/src/main/java/global/fujitsu/persistence/dao/impl/JdbcRegionDao.java
@@ -0,0 +1,52 @@
+package global.fujitsu.persistence.dao.impl;
+
+import global.fujitsu.api.entity.model.region.RegionEntity;
+import global.fujitsu.api.model.region.RegionName;
+import global.fujitsu.api.model.region.WmoCode;
+import global.fujitsu.api.repository.region.RegionRepository;
+import global.fujitsu.persistence.dao.BaseJdbcDao;
+import global.fujitsu.persistence.util.SqlConstants;
+import lombok.NonNull;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public final class JdbcRegionDao
+    extends BaseJdbcDao<RegionEntity>
+    implements RegionRepository {
+
+    public JdbcRegionDao(@NonNull JdbcTemplate jdbcTemplate) {
+        super(
+            jdbcTemplate,
+            "regions",
+            List.of("name", "wmo_code"),
+            RegionEntity.class
+        );
+    }
+
+    @Override
+    public Optional<WmoCode> findWmoCodeByRegionName(@NonNull RegionName regionName) {
+        String sql = SqlConstants.FIND_BY_QUERY("regions", "wmo_code");
+        return jdbcTemplate.query(sql, mapper, regionName.value())
+            .stream().findFirst().map(RegionEntity::wmoCode);
+    }
+
+    @Override
+    public Optional<RegionName> findRegionNameByCode(@NonNull WmoCode wmoCode) {
+        String sql = SqlConstants.FIND_BY_QUERY("regions", "name");
+        return jdbcTemplate.query(sql, mapper, wmoCode.value())
+            .stream().findFirst().map(RegionEntity::name);
+    }
+
+    @Override
+    protected PreparedStatement prepareSaveStatement(PreparedStatement ps, RegionEntity entity) throws SQLException {
+        ps.setString(1, entity.name().value());
+        ps.setString(2, entity.wmoCode().value());
+        return ps;
+    }
+}
