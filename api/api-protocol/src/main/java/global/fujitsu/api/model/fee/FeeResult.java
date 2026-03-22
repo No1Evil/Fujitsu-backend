bf6429ad3d823a5e0096b2374401c39f893af24b
NotEvil <notevil036@gmail.com>
2026-03-23 00:25:52 +0200

    feat(api-protocol): FeeResult implement GetResponse

--- api/api-protocol/src/main/java/global/fujitsu/api/model/fee/FeeResult.java
+++ api/api-protocol/src/main/java/global/fujitsu/api/model/fee/FeeResult.java
@@ -1,8 +1,9 @@
 package global.fujitsu.api.model.fee;
 
+import global.fujitsu.api.model.dto.response.base.GetResponse;
 import lombok.NonNull;
 
 import java.math.BigDecimal;
 
-public record FeeResult(@NonNull BigDecimal fee, @NonNull Boolean isAllowed) {
+public record FeeResult(@NonNull BigDecimal fee, @NonNull Boolean isAllowed) implements GetResponse {
 }
