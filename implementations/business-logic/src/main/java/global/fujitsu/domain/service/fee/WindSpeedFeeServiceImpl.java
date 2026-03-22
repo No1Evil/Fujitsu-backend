697d7c0b18bcdafe4fe2dd08fd1fedf153c3d159
NotEvil <notevil036@gmail.com>
2026-03-23 01:55:36 +0200

    feat(business-logic): WindSpeedFeeServiceImpl implementation matches WindSpeedFeeService

--- implementations/business-logic/src/main/java/global/fujitsu/domain/service/fee/WindSpeedFeeServiceImpl.java
+++ implementations/business-logic/src/main/java/global/fujitsu/domain/service/fee/WindSpeedFeeServiceImpl.java
@@ -2,8 +2,9 @@ package global.fujitsu.domain.service.fee;
 
 import global.fujitsu.api.domain.exceptions.FeeNotFoundException;
 import global.fujitsu.api.domain.exceptions.RestrictedConditionException;
-import global.fujitsu.api.domain.service.WindSpeedFeeService;
+import global.fujitsu.api.domain.service.fee.WindSpeedFeeService;
 import global.fujitsu.api.model.dto.request.create.CreateWindSpeedFeeRequest;
+import global.fujitsu.api.model.dto.request.get.GetWindSpeedFeeRequest;
 import global.fujitsu.api.model.dto.response.get.WindSpeedFeeResponse;
 import global.fujitsu.api.model.fee.FeeResult;
 import global.fujitsu.api.model.vehicle.VehicleType;
@@ -16,22 +17,23 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class WindSpeedFeeServiceImpl implements WindSpeedFeeService {
     private final WindSpeedFeeRepository repository;
     private final WindSpeedFeeMapper mapper;
 
     @Override
+    @Transactional
     public Long create(@NonNull CreateWindSpeedFeeRequest request) {
         var entity = mapper.toEntity(request);
         return repository.save(entity);
     }
 
     @Override
+    @Transactional
     public boolean delete(@NonNull Long id) {
         return repository.delete(id);
     }
@@ -49,19 +51,19 @@ public class WindSpeedFeeServiceImpl implements WindSpeedFeeService {
     }
 
     @Override
-    public FeeResult getFee(@NonNull VehicleType vehicleType, @NonNull BigDecimal windSpeed) {
-        var feeResult = repository.findBaseFee(vehicleType, windSpeed)
+    public FeeResult getBaseFee(GetWindSpeedFeeRequest request) {
+        var feeResult = repository.findBaseFee(request)
             .orElseThrow(() -> new FeeNotFoundException(
                 "Wind speed fee for vehicle {} and wind speed {} not found",
-                vehicleType,
-                windSpeed
+                request.vehicleTypeId(),
+                request.windSpeed()
             ));
 
         if (!feeResult.isAllowed()){
             throw new RestrictedConditionException(
                 "Vehicle {} is not allowed during wind speed {}",
-                vehicleType,
-                windSpeed
+                request.vehicleTypeId(),
+                request.windSpeed()
             );
         }
 
