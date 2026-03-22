34ae563ed74002ebcbce75fca5eb4b5fe2c48efa
NotEvil <notevil036@gmail.com>
2026-03-23 00:20:44 +0200

    feat(api-protocol): WeatherPhenomenonFeeResponse changed VehicleType to vehicleTypeId

--- api/api-protocol/src/main/java/global/fujitsu/api/model/dto/response/get/WeatherPhenomenonFeeResponse.java
+++ api/api-protocol/src/main/java/global/fujitsu/api/model/dto/response/get/WeatherPhenomenonFeeResponse.java
@@ -1,13 +1,13 @@
 package global.fujitsu.api.model.dto.response.get;
 
-import global.fujitsu.api.model.vehicle.VehicleType;
+import global.fujitsu.api.model.dto.response.base.GetResponse;
 import lombok.NonNull;
 
 import java.math.BigDecimal;
 
 public record WeatherPhenomenonFeeResponse(
     @NonNull Long id,
-    @NonNull VehicleType vehicleType,
+    @NonNull Long vehicleTypeId,
     @NonNull String weatherPhenomenon,
     @NonNull BigDecimal price,
     @NonNull Boolean isAllowed
