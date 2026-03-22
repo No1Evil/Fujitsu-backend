1df84a1b4dad1101998ef98dc6b3539b4ac23241
NotEvil <notevil036@gmail.com>
2026-03-23 00:13:21 +0200

    refactor(api-persistence): rename from RegionalBaseFeeRepository to RegionalBasedFeeRepository

--- api/api-persistence/src/main/java/global/fujitsu/api/repository/fee/RegionalBasedFeeRepository.java
+++ api/api-persistence/src/main/java/global/fujitsu/api/repository/fee/RegionalBasedFeeRepository.java
@@ -1,7 +1,6 @@
 package global.fujitsu.api.repository.fee;
 
-import global.fujitsu.api.entity.model.fee.RegionalBaseFeeEntity;
-import global.fujitsu.api.entity.model.region.RegionEntity;
+import global.fujitsu.api.entity.model.fee.RegionalBasedFeeEntity;
 import global.fujitsu.api.model.fee.FeeResult;
 import global.fujitsu.api.model.region.RegionName;
 import global.fujitsu.api.model.vehicle.VehicleType;
@@ -9,6 +8,6 @@ import lombok.NonNull;
 
 import java.util.Optional;
 
-public non-sealed interface RegionalBaseFeeRepository extends FeeRepository<RegionalBaseFeeEntity> {
+public interface RegionalBasedFeeRepository extends FeeRepository<RegionalBasedFeeEntity> {
     Optional<FeeResult> findBaseFee(@NonNull RegionName regionName, @NonNull VehicleType vehicleType);
 }
