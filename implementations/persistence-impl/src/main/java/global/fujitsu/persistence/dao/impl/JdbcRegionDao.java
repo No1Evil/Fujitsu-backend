b617e36668f0e0d69105fdbf563e004813e99fa4
NotEvil <notevil036@gmail.com>
2026-03-23 02:12:27 +0200

    refactor(persistence-impl): JdbcRegionDao changed imports

--- implementations/persistence-impl/src/main/java/global/fujitsu/persistence/dao/impl/JdbcRegionDao.java
+++ implementations/persistence-impl/src/main/java/global/fujitsu/persistence/dao/impl/JdbcRegionDao.java
@@ -4,7 +4,7 @@ import global.fujitsu.api.entity.model.region.RegionEntity;
 import global.fujitsu.api.model.region.RegionName;
 import global.fujitsu.api.model.region.WmoCode;
 import global.fujitsu.api.repository.region.RegionRepository;
-import global.fujitsu.persistence.dao.BaseJdbcDao;
+import global.fujitsu.persistence.dao.base.BaseJdbcDao;
 import global.fujitsu.persistence.util.SqlConstants;
 import lombok.NonNull;
 import org.springframework.jdbc.core.JdbcTemplate;
