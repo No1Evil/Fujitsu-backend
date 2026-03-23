7948c1d470758a208e18b80615d0c351ef162f17
NotEvil <notevil036@gmail.com>
2026-03-23 03:10:29 +0200

    feat(api-domain): implement MeasurementService interface

---
+++ api/api-domain/src/main/java/global/fujitsu/api/domain/service/MeasurementService.java
@@ -0,0 +1,9 @@
+package global.fujitsu.api.domain.service;
+
+
+import global.fujitsu.api.domain.service.base.BaseService;
+import global.fujitsu.api.model.dto.request.create.CreateMeasurementRequest;
+import global.fujitsu.api.model.dto.response.get.MeasurementResponse;
+
+public interface MeasurementService extends BaseService<MeasurementResponse, CreateMeasurementRequest> {
+}
