48672a30a800f6a7a8ce3acc4d6e5222df124e07
NotEvil <notevil036@gmail.com>
2026-03-23 03:09:31 +0200

    feat(api-protocol): GetMeasurementRequest implements GetRequest

--- api/api-protocol/src/main/java/global/fujitsu/api/model/dto/request/get/GetMeasurementRequest.java
+++ api/api-protocol/src/main/java/global/fujitsu/api/model/dto/request/get/GetMeasurementRequest.java
@@ -1,9 +1,10 @@
 package global.fujitsu.api.model.dto.request.get;
 
+import global.fujitsu.api.model.dto.request.base.GetRequest;
 import lombok.NonNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.time.Instant;
 
-public record GetMeasurementRequest(@NonNull Long regionId, @Nullable Instant timestamp) {
+public record GetMeasurementRequest(@NonNull Long regionId, @Nullable Instant timestamp) implements GetRequest {
 }
