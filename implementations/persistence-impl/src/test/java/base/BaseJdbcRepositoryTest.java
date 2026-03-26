e87e20c4d776b1ab4331893d3ca1ac862e25a473
NotEvil <notevil036@gmail.com>
2026-03-26 11:02:35 +0200

    test(persistence-impl): implement testFindAll() in BaseJdbcRepositoryTest

--- implementations/persistence-impl/src/test/java/base/BaseJdbcRepositoryTest.java
+++ implementations/persistence-impl/src/test/java/base/BaseJdbcRepositoryTest.java
@@ -1,9 +1,12 @@
 package base;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 
 import global.fujitsu.api.entity.model.EntityModel;
 import global.fujitsu.api.repository.base.Repository;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,6 +66,37 @@ public abstract class BaseJdbcRepositoryTest<
     assertThat("Entity should have been deleted", repository.findById(entityId2).isEmpty());
 
     assertThat("Repository should be empty after deletion", repository.findAll().isEmpty());
+  }
+
+  @Test
+  void testFindAll(){
+    E testEntity = createTestEntity();
+
+    Long entityId1 = repository.save(testEntity);
+    Long entityId2 = repository.save(testEntity);
+    Long entityId3 = repository.save(testEntity);
+
+    // List isn't too big so it is better to use ArrayList
+    // O(1)
+    List<Long> foundIds = new ArrayList<>();
+    for (var entity : repository.findAll()){
+      foundIds.add(entity.id());
+    }
+
+    hasSize(foundIds.size());
+
+    assertThat(
+        String.format("Entity should have been saved with id %s", entityId1),
+        foundIds.contains(entityId1)
+    );
+    assertThat(
+        String.format("Entity should have been saved with id %s", entityId2),
+        foundIds.contains(entityId2)
+    );
+    assertThat(
+        String.format("Entity should have been saved with id %s", entityId3),
+        foundIds.contains(entityId3)
+    );
   }
 
 
