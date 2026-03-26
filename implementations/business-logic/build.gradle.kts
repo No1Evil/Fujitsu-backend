dbeb999c6395ac1ec6d23f99eb2e8b73f6ca1352
NotEvil <notevil036@gmail.com>
2026-03-26 13:56:19 +0200

    chore(business-logic): add testImplementation of spring-boot-test-autoconfigure

--- implementations/business-logic/build.gradle.kts
+++ implementations/business-logic/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.3"
+    id("org.springframework.boot") version "4.0.4"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -13,4 +13,6 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot")
     implementation("org.springframework:spring-tx")
+
+    testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
 }
