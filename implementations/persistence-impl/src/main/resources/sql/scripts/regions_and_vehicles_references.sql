c4b125f483c7fd5cb7cdc0653205a0bd6f33214f
NotEvil <notevil036@gmail.com>
2026-03-26 22:56:10 +0200

    feat(persistence-impl): regions_and_vehicles_references.sql add additional insert

--- implementations/persistence-impl/src/main/resources/sql/scripts/regions_and_vehicles_references.sql
+++ implementations/persistence-impl/src/main/resources/sql/scripts/regions_and_vehicles_references.sql
@@ -1,2 +1,3 @@
 INSERT INTO regions (id, name, WMO_CODE) VALUES (1, 'Tallinn', '21211');
 INSERT INTO vehicle_types (id, type) VALUES (1, 'Car');
+INSERT INTO vehicle_types (id, type) VALUES (4, 'Test');
