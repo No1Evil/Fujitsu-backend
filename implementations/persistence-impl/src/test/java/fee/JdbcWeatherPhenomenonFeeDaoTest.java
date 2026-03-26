6919362ab6a5fe961563904056871d7a7475ec39
NotEvil <notevil036@gmail.com>
2026-03-26 13:10:05 +0200

    test(persistence-impl): implement JdbcWeatherPhenomenonFeeDaoTest

---
+++ implementations/persistence-impl/src/test/java/fee/JdbcWeatherPhenomenonFeeDaoTest.java
@@ -0,0 +1,45 @@
+package fee;
+
+import base.BaseJdbcBasedFeeRepositoryTest;
+import global.fujitsu.api.entity.model.fee.WeatherPhenomenonFeeEntity;
+import global.fujitsu.api.model.dto.request.get.GetWeatherPhenomenonFeeRequest;
+import global.fujitsu.persistence.dao.impl.fee.JdbcWeatherPhenomenonFeeDao;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.test.autoconfigure.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+@JdbcTest
+@Sql("/sql/regions_and_vehicles_references.sql")
+@Import(JdbcWeatherPhenomenonFeeDao.class)
+public class JdbcWeatherPhenomenonFeeDaoTest
+    extends BaseJdbcBasedFeeRepositoryTest<
+    JdbcWeatherPhenomenonFeeDao, GetWeatherPhenomenonFeeRequest, WeatherPhenomenonFeeEntity> {
+
+  @Autowired
+  public JdbcWeatherPhenomenonFeeDaoTest(JdbcWeatherPhenomenonFeeDao repository) {
+    super(repository);
+  }
+
+  @Override
+  public GetWeatherPhenomenonFeeRequest createPassingRequest() {
+    return new GetWeatherPhenomenonFeeRequest(1L, "rain");
+  }
+
+  @Override
+  public GetWeatherPhenomenonFeeRequest createOutOfBoundRequest() {
+    return new GetWeatherPhenomenonFeeRequest(1L, "storm");
+  }
+
+  @Override
+  protected WeatherPhenomenonFeeEntity createTestEntity() {
+    return new WeatherPhenomenonFeeEntity(
+        null,
+        1L,
+        "rain",
+        BigDecimal.ZERO,
+        true
+    );
+  }
+}
