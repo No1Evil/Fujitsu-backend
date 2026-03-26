18f3513c1a3903d130b078bde9034da771b49279
NotEvil <notevil036@gmail.com>
2026-03-26 13:06:32 +0200

    test(persistence-impl): implement JdbcAirTemperatureFeeDaoTest

---
+++ implementations/persistence-impl/src/test/java/fee/JdbcAirTemperatureFeeDaoTest.java
@@ -0,0 +1,46 @@
+package fee;
+
+import base.BaseJdbcBasedFeeRepositoryTest;
+import global.fujitsu.api.entity.model.fee.AirTemperatureFeeEntity;
+import global.fujitsu.api.model.dto.request.get.GetAirTemperatureFeeRequest;
+import global.fujitsu.persistence.dao.impl.fee.JdbcAirTemperatureFeeDao;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.test.autoconfigure.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+@JdbcTest
+@Sql("/sql/regions_and_vehicles_references.sql")
+@Import(JdbcAirTemperatureFeeDao.class)
+public class JdbcAirTemperatureFeeDaoTest
+    extends BaseJdbcBasedFeeRepositoryTest<
+    JdbcAirTemperatureFeeDao, GetAirTemperatureFeeRequest, AirTemperatureFeeEntity> {
+
+  @Autowired
+  public JdbcAirTemperatureFeeDaoTest(JdbcAirTemperatureFeeDao repository) {
+    super(repository);
+  }
+
+  @Override
+  public GetAirTemperatureFeeRequest createPassingRequest() {
+    return new GetAirTemperatureFeeRequest(1L, BigDecimal.valueOf(5L));
+  }
+
+  @Override
+  public GetAirTemperatureFeeRequest createOutOfBoundRequest() {
+    return new GetAirTemperatureFeeRequest(1L, BigDecimal.valueOf(100L));
+  }
+
+  @Override
+  protected AirTemperatureFeeEntity createTestEntity() {
+    return new AirTemperatureFeeEntity(
+        null,
+        1L,
+        BigDecimal.valueOf(5L),
+        BigDecimal.valueOf(10L),
+        BigDecimal.valueOf(1L),
+        true
+    );
+  }
+}
