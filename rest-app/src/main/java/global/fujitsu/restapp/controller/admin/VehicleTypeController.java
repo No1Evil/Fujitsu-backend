fbd9b2d4c4edaeb8e932b424732d0b9793c513e7
NotEvil <notevil036@gmail.com>
2026-03-27 23:13:24 +0200

    refactor(rest-app): move VehicleTypeController to /controller/admin

--- rest-app/src/main/java/global/fujitsu/restapp/controller/admin/VehicleTypeController.java
+++ rest-app/src/main/java/global/fujitsu/restapp/controller/admin/VehicleTypeController.java
@@ -1,4 +1,4 @@
-package global.fujitsu.restapp.controller;
+package global.fujitsu.restapp.controller.admin;
 
 import global.fujitsu.api.domain.service.VehicleTypeService;
 import global.fujitsu.api.model.dto.request.create.CreateVehicleTypeRequest;
@@ -9,24 +9,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Provides API for vehicle types. */
 @RestController
-@RequestMapping("api/v1/admin/regions")
+@RequestMapping(value = "/api/admin/vehicles", headers = "X-API-VERSION=1")
 @RequiredArgsConstructor
 public class VehicleTypeController {
 
   private final VehicleTypeService service;
 
-  /** {@return found vehicle type} */
+  /** {@return found vehicle type or all vehicles} */
   @GetMapping
-  public ResponseEntity<VehicleTypeResponse> findByName(@RequestBody VehicleType vehicleType) {
-    return ResponseEntity.ok(service.findByName(vehicleType));
+  public ResponseEntity<?> find(@RequestBody(required = false) VehicleType vehicleType) {
+    if (vehicleType != null) {
+      return ResponseEntity.ok(service.findByName(vehicleType));
+    }
+    return ResponseEntity.ok(service.findAll());
   }
 
   /** {@return created vehicle type id} */
@@ -36,20 +39,14 @@ public class VehicleTypeController {
   }
 
   /** {@return is vehicle type deleted} */
-  @DeleteMapping
-  public ResponseEntity<Boolean> delete(@RequestParam Long id) {
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Boolean> delete(@PathVariable Long id) {
     return ResponseEntity.ok(service.delete(id));
   }
 
   /** {@return found vehicle type} */
-  @GetMapping
-  public ResponseEntity<VehicleTypeResponse> findById(@RequestParam Long id) {
+  @GetMapping("/{id}")
+  public ResponseEntity<VehicleTypeResponse> findById(@PathVariable Long id) {
     return ResponseEntity.ok(service.findById(id));
-  }
-
-  /** {@return found vehicle type list} */
-  @GetMapping
-  public ResponseEntity<List<VehicleTypeResponse>> findAll() {
-    return ResponseEntity.ok(service.findAll());
   }
 }
