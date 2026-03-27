89600eb11794b372af674fc4f24fd53cab142530
NotEvil <notevil036@gmail.com>
2026-03-27 03:05:56 +0200

    feat(rest-app): implement RestApplication starter

---
+++ rest-app/src/main/java/global/fujitsu/restapp/RestApplication.java
@@ -0,0 +1,11 @@
+package global.fujitsu.restapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "global.fujitsu")
+public class RestApplication {
+  static void main(String[] args){
+    SpringApplication.run(RestApplication.class, args);
+  }
+}
