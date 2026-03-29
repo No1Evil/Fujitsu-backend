bfad2ca5f463bd1510bb5dbac7939561ff8b7d0e
NotEvil <notevil036@gmail.com>
2026-03-29 02:44:25 +0200

    feat(rest-app): implement MeasurementSyncService

---
+++ rest-app/src/main/java/global/fujitsu/restapp/domain/service/MeasurementSyncService.java
@@ -0,0 +1,68 @@
+package global.fujitsu.restapp.domain.service;
+
+import global.fujitsu.api.domain.service.MeasurementService;
+import global.fujitsu.api.domain.service.RegionService;
+import global.fujitsu.api.model.dto.request.create.CreateMeasurementRequest;
+import global.fujitsu.restapp.domain.parser.MeasurementParser;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Synchronizes the measurements with
+ * <a href="https://www.ilmateenistus.ee">ilmateenistus</a>.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@EnableScheduling
+public class MeasurementSyncService {
+
+  private final RegionService regionService;
+  private final MeasurementService measurementService;
+  private final MeasurementParser measurementParser;
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  private final Map<String, Long> regionMap = new ConcurrentHashMap<>();
+
+  /** Synchronizes measurements in the database. */
+  @Scheduled(cron = "0 15 * * * *")
+  public void sync() {
+    log.info("started synchronizing");
+    String url = "https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php";
+
+    // Checking regions if new ones were added or deleted
+    refreshRegions();
+
+    // Fetching xml and parsing
+    restTemplate.execute(url, HttpMethod.GET, null, response -> {
+      ArrayList<CreateMeasurementRequest> requests = null;
+      try {
+        requests = measurementParser.parse(response.getBody(), regionMap);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+      requests.forEach(measurementService::create);
+      return null;
+    });
+  }
+
+  /**
+   * Refresh region map and insert all.
+   */
+  private void refreshRegions() {
+    regionMap.clear();
+
+    for (var region : regionService.findAll()) {
+      String regionName = region.regionName().value();
+      regionMap.put(regionName, region.id());
+    }
+  }
+}
