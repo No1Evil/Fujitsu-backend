74cdb85941d5c1eee424042d323b72e25b6e085c
NotEvil <notevil036@gmail.com>
2026-03-30 15:18:22 +0300

    fix(persistence-impl): switched from insert to merge in regions_and_vehicles_references.sql for tests not to fail

--- implementations/persistence-impl/src/main/resources/sql/scripts/regions_and_vehicles_references.sql
+++ implementations/persistence-impl/src/main/resources/sql/scripts/regions_and_vehicles_references.sql
@@ -1,3 +1,4 @@
-insert into regions (id, name, WMO_CODE) values (1, 'Tallinn', '21211');
-insert into vehicle_types (id, type) values ( 1, 'Car');
-insert into vehicle_types (id, type) values (4, 'Test');
+merge into regions (id, name, WMO_CODE) values (1, 'Tallinn', '21211');
+merge into vehicle_types (id, type) values ( 1, 'Car');
+merge into vehicle_types (id, type) values (4, 'Test');
+ALTER TABLE regions ALTER COLUMN id RESTART WITH 4;
